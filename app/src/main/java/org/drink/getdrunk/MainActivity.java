09960f0c8c0c2e2a35341c22c4faed258ca9c06c
Zaiser Benjamin (INST-IOT/ESW-Wa) <Benjamin.Zaiser@bosch-si.com>
2016-11-04 21:38:50 +0100

    initial commit with Hello World app

---
+++ app/src/main/java/org/drink/getdrunk/MainActivity.java
@@ -0,0 +1,10 @@
+package org.drink.getdrunk;
+
+import org.androidannotations.annotations.EActivity;
+import android.support.v7.app.AppCompatActivity;
+
+@EActivity( R.layout.activity_main )
+public class MainActivity extends AppCompatActivity {
+
+
+}
